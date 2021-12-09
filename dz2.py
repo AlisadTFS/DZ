ef136d795993dff464ad3dfae2cb9be6be9c6ea5
AlisadTFS <virdata@yandex.ru>
2021-12-09 23:53:19 +0800

    Эксперемент

--- dz2.py
+++ dz2.py
@@ -2,18 +2,21 @@ i = 1
 ls_res = []
 sum_res = 0
 sum_q = 0
+# def sum_numb (number):
+#     sum_number = 0
+#     for i in str(number):
+#         sum_number = sum_number + int(i)
+#     return sum_number
 while i < 1000:
     res = i**3
     ls_res.append(res)
     sum_q = 0
     for q in str(res):
         sum_q = sum_q + int(q)
+    #sum_q = sum_numb(res)
     if sum_q % 7 == 0:
         sum_res = sum_res + res
     i = i + 2
-# for s in ls_res:
-#     sum_q = 0
-#         ls_res1.append(sum_q)
 print(ls_res)
 print(sum_res)
 for numb in ls_res:
